9f44073be9d654f8c29cc74a3fd5f07c4fdaaac2
BjornTheThunder <177275210+BjornTheThunder@users.noreply.github.com>
2026-03-04 10:42:40 +0100

    Created modal for adding a Task
    Created the modal with the functions for adding a new Task, inside the new task.js file

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
-import { activeProjectId, projectList } from "./project.js";
+import { activeProjectId, getProjectById, projectList } from "./project.js";
+import { addNewTask } from "./task.js";
 import { updateUI } from "./ui.js";
+
+const newTaskBtn = document.getElementById("new-task-button");
 
 projectList.addEventListener("click", (event) => {
   const projectItem = event.target.closest(".project-item");
@@ -11,5 +14,8 @@ projectList.addEventListener("click", (event) => {
   }
 });
 
+newTaskBtn.addEventListener('click', () => {
+  addNewTask();
+});
 
 updateUI();
---
+++ src/task.js
@@ -0,0 +1,50 @@
+import { activeProjectId, getProjectById } from "./project.js";
+import { updateUI } from "./ui.js";
+
+const taskModal = document.getElementById('taskModalOverlay');
+const taskTitle = document.getElementById('taskTitleInput');
+const taskDesc = document.getElementById('taskDescInput');
+const taskDate = document.getElementById('taskDateInput');
+const saveTaskBtn = document.getElementById('saveTaskBtn');
+const closeTaskBtn = document.getElementById('closeTaskModal');
+export const newTaskBtn = document.getElementById("new-task-button");
+
+export function addNewTask() {
+  const currentProject = getProjectById(activeProjectId);
+  if (!currentProject) {
+    alert("Please select or create a project first!");
+    return;
+  }
+  document.getElementById('targetProjectName').textContent = currentProject.title;
+  taskModal.classList.add('active');
+}
+
+saveTaskBtn.addEventListener('click', () => {
+  const title = taskTitle.value.trim();
+  const desc = taskDesc.value.trim();
+  const date = taskDate.value;
+
+  if (title && desc && date) {
+    const currentProject = getProjectById(activeProjectId);
+
+    const newTask = {
+      title: title,
+      description: desc,
+      dueDate: date,
+      notes: []
+    };
+
+    currentProject.items.push(newTask);
+
+    updateUI();
+
+    taskModal.classList.remove('active');
+    [taskTitle, taskDesc, taskDate].forEach(el => el.value = '');
+  } else {
+    alert("Please fill in all fields to create a task.");
+  }
+});
+
+closeTaskBtn.addEventListener('click', () => {
+  taskModal.classList.remove('active');
+});
